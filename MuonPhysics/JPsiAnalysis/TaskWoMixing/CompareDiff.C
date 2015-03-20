2361f1f7f776761b0d6eb8e674f06de62c328069
ppillot <pillot@subatech.in2p3.fr>
2015-03-20 17:34:52 +0100

    new comparisons

--- MuonPhysics/JPsiAnalysis/TaskWoMixing/CompareDiff.C
+++ MuonPhysics/JPsiAnalysis/TaskWoMixing/CompareDiff.C
@@ -101,23 +101,62 @@ void CompareDiff()
     "w/   pDCA"
   };
   */
-  const Int_t nFiles = 4;
+/*  const Int_t nFiles = 7;
   TString fileName[nFiles] = {
     "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_MB_TrgSign.root",
     "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_MB_TrgSign_relabeled.root",
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_woDecays_MB_TrgSign_relabeled_woDecays.root",
     "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_10sigma_MB_TrgSign_relabeled_10sigma.root",
-    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_25sigma_MB_TrgSign_relabeled_25sigma.root"
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_10sigma_woDecays_MB_TrgSign_relabeled_10sigma_woDecays.root",
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_25sigma_MB_TrgSign_relabeled_25sigma.root",
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsi/Diff_MB_relabeled_25sigma_woDecays_MB_TrgSign_relabeled_25sigma_woDecays.root"
   };
   TString legend[nFiles] = {
     "label reco",
     "relabeled",
+    "relabeledwoD",
     "relabeled10",
-    "relabeled25"
+    "relabeled10woD",
+    "relabeled25",
+    "relabeled25woD"
+  };
+  */
+  const Int_t nFiles = 3;
+/*  TString fileName[nFiles] = {
+    "/Users/pillot/Work/Alice/Data/2011/LHC11h/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11h/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign2_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11h/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign3_2.81-3.29.root"
+  };*/
+/*  TString fileName[nFiles] = {
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign2_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign3_2.81-3.29.root"
+  };*/
+/*  TString fileName[nFiles] = {
+     "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign_9.01-9.99.root",
+     "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign2_9.01-9.99.root",
+     "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign3_9.01-9.99.root"
+  };*/
+  TString fileName[nFiles] = {
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign_0.91-1.19.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign2_0.91-1.19.root",
+    "/Users/pillot/Work/Alice/Data/2011/LHC11cd/pass2_muon/JPsipDCAv2/Diff_MULorMLL_MULorMLL_TrgSign3_0.91-1.19.root"
+  };
+/*  TString fileName[nFiles] = {
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsipDCAv2/Diff_MB_MB_TrgSign_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsipDCAv2/Diff_MB_MB_TrgSign2_2.81-3.29.root",
+    "/Users/pillot/Work/Alice/Sim/LHC11h/EmbeddingPass2/JPsipDCAv2/Diff_MB_MB_TrgSign3_2.81-3.29.root"
+  };*/
+  TString legend[nFiles] = {
+    "dev #pm 0",
+    "dev #pm 1",
+    "dev #pm 2"
   };
   
-  Int_t lineColor[5] = {1, 2, 4, 8, 6};
-  
-  TString histoName[16] = {
+  Int_t lineColor[8] = {1, 2, 4, 8, 6, 3, 5, 7};
+  /*
+  const Int_t nHist = 16;
+  TString histoName[nHist] = {
     "hDiffVsCent",
     "hDiffVsCentpT1",
     "hDiffVsCentpT2",
@@ -135,6 +174,12 @@ void CompareDiff()
     "hDiffVsyCent2",
     "hDiffVsyCent4"
   };
+  */
+  const Int_t nHist = 2;
+  TString histoName[nHist] = {
+    "hDiffVspT",
+    "hDiffVsy"
+  };
   
   TLegend *l = new TLegend(0.7,0.7,0.9,0.9);
   l->SetFillStyle(0);
@@ -143,13 +188,13 @@ void CompareDiff()
   TH1F *h[nFiles][16];
   for (Int_t i = 0; i < nFiles; ++i) {
     TFile *file = TFile::Open(fileName[i].Data(),"READ");
-    for (Int_t j = 0; j < 16; ++j) {
+    for (Int_t j = 0; j < nHist; ++j) {
       h[i][j] = static_cast<TH1F*>(file->FindObjectAny(histoName[j].Data()));
     }
     l->AddEntry(h[i][0],legend[i].Data(),"l");
   }
   
-  for (Int_t j = 0; j < 16; ++j) {
+  for (Int_t j = 0; j < nHist; ++j) {
     TCanvas* c = new TCanvas(Form("c%s",histoName[j].Data()), h[0][j]->GetTitle(), 800, 400);
     for (Int_t i = 0; i < nFiles; ++i) {
       h[i][j]->SetLineColor(lineColor[i]);
