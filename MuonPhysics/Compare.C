35e9cc7304677d0e652940a953b4e4709fcd625c
ppillot <pillot@subatech.in2p3.fr>
2015-10-20 15:31:07 +0200

    loading of libraries not needed anymore

--- MuonPhysics/Compare.C
+++ MuonPhysics/Compare.C
@@ -16,13 +16,10 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   gROOT->ForceStyle();
   gStyle->SetOptFit(1);
   
-  // prepare environment
-  TString extraLibs = "RAWDatabase:CDB:STEER:MUONcore:MUONmapping:MUONcalib:MUONgeometry:MUONtrigger:MUONraw:MUONbase:MUONrec";
-  LoadAlirootLocally(extraLibs);
-  
   Bool_t sameFile = (fileName_before==fileName_after);
 //  TString extBefore = sameFile ? "_before" : "_after";
-  TString extBefore = "_phys";
+//  TString extBefore = "_after";
+  TString extBefore = "";
 //  TString extAfter = "_after";
   TString extAfter = "";
   
@@ -55,17 +52,17 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   AliCounterCollection* event_before = static_cast<AliCounterCollection*>(file_before->FindObjectAny(Form("eventCounters%s",extBefore.Data())));
   if (!event_before) return;
   Double_t nEventsBefore = event_before->GetSum("event:all");
-  printf("\n# events before = %d\n",(Int_t)nEventsBefore);
+  printf("\n# events before = %ld\n",(Long_t)nEventsBefore);
   
   // number of events after
   AliCounterCollection* event_after = static_cast<AliCounterCollection*>(file_after->FindObjectAny(Form("eventCounters%s",extAfter.Data())));
   if (!event_after) return;
   Double_t nEventsAfter = event_after->GetSum("event:all");
-  printf("# events after = %d\n",(Int_t)nEventsAfter);
+  printf("# events after = %ld\n",(Long_t)nEventsAfter);
   
   // draw differences
   const Int_t nHists = 16;
-  TString sHist[nHists] = {"hChi2", "hNClustersPerTrack", "hNChamberHitPerTrack", "hNTracks", "hDCA", "hPt", "hRapidity", "hNTrigAll", "hMass", "hPUncorrected", "hRAbs", "hNMatchTracks", "hPtMuPlus", "hPtMuMinus", "hChi2Trig", "hMultSelect"};
+  TString sHist[nHists] = {"hChi2", "hNClustersPerTrack", "hNChamberHitPerTrack", "hNTracks", "hDCA", "hPt", "hRapidity", "hNTrigAll", /*"hPDCA23","hPDCA310",*/"hMass", /*"hPUncorrected"*/"hPtDimu", "hRAbs", "hNMatchTracks", "hPtMuPlus", "hPtMuMinus", "hChi2Trig", "hMultSelect"};
   TCanvas* cHist = new TCanvas("cHist", "cHist", 1200, 1100);
   cHist->Divide(4,4);
   TLegend *lHist = new TLegend(0.5,0.65,0.9,0.8);
@@ -170,32 +167,5 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   printf("highPt:  %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
   printf("\n");
   */
-}
-
-//______________________________________________________________________________
-void LoadAlirootLocally(TString& extraLibs)
-{
-  /// Load libraries locally
-  
-  // Load common libraries
-  gSystem->Load("libVMC");
-  gSystem->Load("libTree.so");
-  gSystem->Load("libPhysics.so");
-  gSystem->Load("libMinuit.so");
-  gSystem->Load("libXMLParser.so");
-  gSystem->Load("libGui.so");
-  gSystem->Load("libSTEERBase");
-  gSystem->Load("libESD");
-  gSystem->Load("libAOD");
-  gSystem->Load("libANALYSIS");
-  gSystem->Load("libANALYSISalice");
-  
-  // Load additional libraries
-  gSystem->Load("libProofPlayer");
-  TObjArray* libs = extraLibs.Tokenize(":");
-  for (Int_t i = 0; i < libs->GetEntriesFast(); i++)
-    gSystem->Load(Form("lib%s",static_cast<TObjString*>(libs->UncheckedAt(i))->GetName()));
-  delete libs;
-  
 }
 
