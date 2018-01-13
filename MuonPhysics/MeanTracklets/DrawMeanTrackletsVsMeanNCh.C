cc60f639e596cb9be1d79eb46a0284d3b8e972f3
ppillot <pillot@subatech.in2p3.fr>
2018-01-13 19:07:06 +0100

    add plots for Nch vs Zvtx

--- MuonPhysics/MeanTracklets/DrawMeanTrackletsVsMeanNCh.C
+++ MuonPhysics/MeanTracklets/DrawMeanTrackletsVsMeanNCh.C
@@ -17,21 +17,36 @@ void DrawMeanTrackletsVsMeanNCh(TString fileNameData = "AnalysisResults.root", B
   THnSparse *fhNtrk = static_cast<THnSparse*>(file->FindObjectAny(corr ? "hNtrkCorr" : "hNtrk"));
   if (!fhNtrk) return;
   
+//  fhNtrk->GetAxis(0)->SetRangeUser(30, 40);
+  
   TH2D *hNchVsNtrk = fhNtrk->Projection(3,0,"e");
   hNchVsNtrk->SetDirectory(0);
   TString pName = corr ? "pMeanNchVsNtrkCorr" : "pMeanNchVsNtrk";
   TProfile *pMeanNchVsNtrk = hNchVsNtrk->ProfileX(pName.Data());
   pMeanNchVsNtrk->SetDirectory(0);
   
+  TH2D *hNchVsZvtx = fhNtrk->Projection(3,1,"e");
+  hNchVsZvtx->SetDirectory(0);
+  TProfile *pMeanNchVsZvtx = hNchVsZvtx->ProfileX("pMeanNchVsZvtx");
+  pMeanNchVsZvtx->SetDirectory(0);
+  
   file->Close();
   delete file;
   
-  // draw histogram
+  // draw histograms
   TString cName = corr ? "cDCCorr" : "cDC";
   TCanvas *c = new TCanvas(cName.Data(), cName.Data());
   gPad->SetLogz();
   hNchVsNtrk->Draw("colz");
   pMeanNchVsNtrk->Draw("sames");
+  
+  TCanvas *c2 = new TCanvas("cNchVsZvtx", "cNchVsZvtx");
+  c2->Divide(2,1);
+  c2->cd(1);
+  gPad->SetLogz();
+  hNchVsZvtx->Draw("colz");
+  c2->cd(2);
+  pMeanNchVsZvtx->Draw("");
   
 }
 
