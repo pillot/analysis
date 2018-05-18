5a82dddc16ea977a174b93868bb8bcdd2356c348
ppillot <pillot@subatech.in2p3.fr>
2018-05-18 13:44:25 +0200

    new option to rebin histograms

--- MuonPhysics/Compare.C
+++ MuonPhysics/Compare.C
@@ -7,7 +7,7 @@
  *
  */
 
-void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_after = "AnalysisResults.root")
+void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_after = "AnalysisResults.root", Bool_t rebin = kFALSE)
 {
   /// compare results before/after refit if fileName_before==fileName_after
   /// or after refit in both files if they are different
@@ -17,10 +17,7 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   gStyle->SetOptFit(1);
   
   Bool_t sameFile = (fileName_before==fileName_after);
-//  TString extBefore = sameFile ? "_before" : "_after";
-//  TString extBefore = "_after";
   TString extBefore = "";
-//  TString extAfter = "_after";
   TString extAfter = "";
   
   // open files
@@ -62,7 +59,8 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   
   // draw differences
   const Int_t nHists = 16;
-  TString sHist[nHists] = {"hChi2", "hNClustersPerTrack", "hNChamberHitPerTrack", "hNTracks", "hDCA", "hPt", "hRapidity", "hNTrigAll", /*"hPDCA23","hPDCA310",*/"hMass", /*"hPUncorrected"*/"hPtDimu", "hRAbs", "hNMatchTracks", "hPtMuPlus", "hPtMuMinus", "hChi2Trig", "hMultSelect"};
+  TString sHist[nHists] = {"hChi2", "hNClustersPerTrack", "hNChamberHitPerTrack", "hNTracks", "hDCA", "hPt", "hRapidity", "hNTrigAll", /*"hPDCA23","hPDCA310",*/"hMass", /*"hPUncorrected"*/"hPtDimu", "hRAbs", "hNMatchTracks", "hPtMuPlus", "hPtMuMinus", "hChi2Trig", /*"hMultSelect"*/"hCent"};
+  Int_t nRebin[nHists] = {4, 1, 1, 1, 10, 5, 8, 1, 16, 5, 10, 1, 5, 5, 4, 1};
   TCanvas* cHist = new TCanvas("cHist", "cHist", 1200, 1100);
   cHist->Divide(4,4);
   TLegend *lHist = new TLegend(0.5,0.65,0.9,0.8);
@@ -83,12 +81,26 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
     gPad->SetLogy();
     TH1F* h_before = static_cast<TH1F*>(histo_before->FindObject(sHist[i].Data()));
     if (!h_before) continue;
-    //if (i == 15) h_before->Rebin(10);
-    //h_before->Scale(1./nEventsBefore);
-    TH1F* h_after = static_cast<TH1F*>(histo_after->FindObject(sHist[i].Data())));
+    h_before->Sumw2();
+    if (rebin) h_before->Rebin(nRebin[i]);
+/*    if (i == 12) {
+      if (rebin) h_before->Rebin(nRebin[i]);
+      TH1F *tmp = static_cast<TH1F*>(histo_before->FindObject(sHist[13].Data()));
+      if (rebin) tmp->Rebin(nRebin[13]);
+      h_before->Divide(tmp);
+    } else if (rebin && i != 13) h_before->Rebin(nRebin[i]);
+*/    //h_before->Scale(1./nEventsBefore);
+    TH1F* h_after = static_cast<TH1F*>(histo_after->FindObject(sHist[i].Data()));
     if (!h_after) continue;
-    //if (i == 15) h_after->Rebin(10);
-    //h_after->Scale(1./nEventsAfter);
+    h_after->Sumw2();
+    if (rebin) h_after->Rebin(nRebin[i]);
+/*    if (i == 12) {
+      if (rebin) h_after->Rebin(nRebin[i]);
+      TH1F *tmp = static_cast<TH1F*>(histo_after->FindObject(sHist[13].Data()));
+      if (rebin) tmp->Rebin(nRebin[13]);
+      h_after->Divide(tmp);
+    } else if (rebin && i != 13) h_after->Rebin(nRebin[i]);
+*/    //h_after->Scale(1./nEventsAfter);
     h_before->Draw();
     h_after->Draw("sames");
     h_after->SetLineColor(2);
@@ -98,7 +110,7 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
       lHist->Draw("same");
     }
     cDiff->cd(i+1);
-    TH1F* h_diff = h_before->Clone();
+    TH1F* h_diff = static_cast<TH1F*>(h_before->Clone());
     h_diff->Add(h_after, -1.);
     h_diff->Draw();
     h_diff->SetLineColor(4);
@@ -107,7 +119,7 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
       lDiff->Draw("same");
     }
     cRatio->cd(i+1);
-    TH1F* h_ratio = h_after->Clone();
+    TH1F* h_ratio = static_cast<TH1F*>(h_after->Clone());
     h_ratio->Divide(h_before);
     h_ratio->Draw();
     h_ratio->SetLineColor(4);
@@ -135,10 +147,16 @@ void Compare(TString fileName_before = "AnalysisResults.root", TString fileName_
   printf(">1GeV:   %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
   b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/pt:2GeV"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/pt:2GeV");
   printf(">2GeV:   %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
+  b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/pt:4GeV"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/pt:4GeV");
+  printf(">4GeV:   %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
   b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:low"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:low");
   printf("lowPt:   %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
+  b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:low/pt:1GeV"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:low/pt:1GeV");
+  printf(">1GeV:   %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
   b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:high"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:high");
   printf("highPt:  %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
+  b = counters_before->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:high/pt:4GeV"); a = counters_after->GetSum("track:matched/rabs:yes/eta:yes/pdca:yes/trig:high/pt:4GeV");
+  printf(">4GeV:  %9d %9d %7.2f %%\n", (Int_t)b, (Int_t)a, 100.*(a-b)/b);
   printf("\n");
   /*
   // print statistic
