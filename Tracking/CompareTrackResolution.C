993208f20d15b9e924e97e35d35043df3d22c252
ppillot <pillot@subatech.in2p3.fr>
2020-06-29 14:49:54 +0200

    macro to compare clusters position and cluster-track residuals

---
+++ Tracking/CompareTrackResolution.C
@@ -0,0 +1,525 @@
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <utility>
+#include <math.h>
+
+#include <TMath.h>
+#include <TStyle.h>
+#include <TCanvas.h>
+#include <TPad.h>
+#include <TLegend.h>
+#include <TH1F.h>
+#include <TF1.h>
+#include <TGraphErrors.h>
+#include <TDatabasePDG.h>
+#include <Math/Vector4D.h>
+
+#include "MCHBase/ClusterBlock.h"
+#include "MCHBase/TrackBlock.h"
+#include "MCHTracking/TrackParam.h"
+#include "MCHTracking/Cluster.h"
+#include "MCHTracking/Track.h"
+#include "/Users/PILLOT/Work/Alice/devO2Tracking/O2/Detectors/MUON/MCH/Tracking/src/TrackFitter.h"
+#include "/Users/PILLOT/Work/Alice/devO2Tracking/O2/Detectors/MUON/MCH/Tracking/src/TrackExtrap.h"
+
+using namespace std;
+using namespace o2::mch;
+using namespace ROOT::Math;
+
+static const double muMass = TDatabasePDG::Instance()->GetParticle("mu-")->Mass();
+double chi2Max = 2. * 4. * 4.;
+TrackFitter trackFitter{};
+
+//_________________________________________________________________________________________________
+struct TrackStruct {
+  PxPyPzMVector pxpypzm{};
+  short sign = 0;
+  double dca = 0.;
+  double rAbs = 0.;
+  double chi2 = 0.;
+  TrackParamStruct param{};
+  std::vector<Cluster> clusters{};
+  Track track{};
+  bool matchFound = false;
+  bool matchIdentical = false;
+
+  //_______________________________________________________________________________________________
+  bool operator==(const TrackStruct& track) const
+  {
+    /// tracks are considered identical when all their clusters match within chi2Max
+    if (this->clusters.size() != track.clusters.size()) {
+      return false;
+    }
+    for (size_t iCl = 0; iCl != this->clusters.size(); ++iCl) {
+      auto& cl1 = this->clusters[iCl];
+      auto& cl2 = track.clusters[iCl];
+      if (cl1.getDEId() != cl2.getDEId()) {
+        return false;
+      }
+      double dx = cl1.getX() - cl2.getX();
+      double dy = cl1.getY() - cl2.getY();
+      double chi2 = dx * dx / (cl1.getEx2() + cl2.getEx2()) + dy * dy / (cl1.getEy2() + cl2.getEy2());
+      if (chi2 > chi2Max) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  //_______________________________________________________________________________________________
+  bool match(const TrackStruct& track) const
+  {
+    /// Try to match this track with the given track. Matching conditions:
+    /// - more than 50% of clusters from one of the two tracks matched with clusters from the other
+    /// - at least 1 cluster matched before and 1 cluster matched after the dipole
+
+    size_t nMatchClusters(0);
+    bool matchCluster[10] = {false, false, false, false, false, false, false, false, false, false};
+
+    for (const auto& cl1 : this->clusters) {
+      for(const auto& cl2 : track.clusters) {
+        if (cl1.getDEId() == cl2.getDEId()) {
+          double dx = cl1.getX() - cl2.getX();
+          double dy = cl1.getY() - cl2.getY();
+          double chi2 = dx * dx / (cl1.getEx2() + cl2.getEx2()) + dy * dy / (cl1.getEy2() + cl2.getEy2());
+          if (chi2 < chi2Max) {
+            matchCluster[cl1.getChamberId()] = true;
+            ++nMatchClusters;
+            break;
+          }
+        }
+      }
+    }
+
+    return ((matchCluster[0] || matchCluster[1] || matchCluster[2] || matchCluster[3]) &&
+            (matchCluster[6] || matchCluster[7] || matchCluster[8] || matchCluster[9]) &&
+            (2 * nMatchClusters > this->clusters.size() || 2 * nMatchClusters > track.clusters.size()));
+  }
+};
+
+//_________________________________________________________________________________________________
+int ReadNextEvent(ifstream& inFile, bool selectTracks, std::vector<TrackStruct>& tracks);
+void ReadTrack(ifstream& inFile, TrackStruct& track);
+bool IsSelected(TrackStruct& track);
+void CompareTracks(std::vector<TrackStruct>& tracks1, std::vector<TrackStruct>& tracks2, std::vector<TH1*>& histos);
+void CreateResiduals(std::vector<TH1*>& histos, const char* extension, double range);
+void FillResiduals(std::vector<TrackStruct>& tracks, std::vector<TH1*>& histos, bool matched = false);
+void FillResiduals(TrackStruct& track1, TrackStruct& track2, std::vector<TH1*>& histos);
+void DrawResiduals(std::vector<TH1*>& histos, const char* extension);
+void DrawResiduals(std::vector<TH1*>& histos1, std::vector<TH1*>& histos2, const char* extension);
+pair<double, double> GetSigma(TH1* h, int color);
+double CrystalBallSymmetric(double* xx, double* par);
+
+//_________________________________________________________________________________________________
+void CompareTrackResolution(string inFileName1, string inFileName2, bool selectTracks = false)
+{
+  /// Compare the cluster-track residuals between the tracks stored in the 2 binary files
+  /// O2 tracking need to be loaded before: gSystem->Load("libO2MCHTracking")
+
+  // open files
+  ifstream inFile1(inFileName1, ios::binary);
+  ifstream inFile2(inFileName2, ios::binary);
+  if (!inFile1.is_open() || !inFile2.is_open()) {
+    cout << "fail opening files" << endl;
+    return;
+  }
+
+  // prepare the track fitter
+  trackFitter.initField(-30000.025391, -5999.954590);
+  trackFitter.smoothTracks(true);
+  TrackExtrap::useExtrapV2();
+
+  int event1(0), event2(0);
+  std::vector<TrackStruct> tracks1{};
+  std::vector<TrackStruct> tracks2{};
+  bool readNextEvent1(true), readNextEvent2(true);
+  std::vector<TH1*> residuals[5] = {{}, {}, {}, {}, {}};
+  CreateResiduals(residuals[0], "All1", 2.);
+  CreateResiduals(residuals[1], "All2", 2.);
+  CreateResiduals(residuals[2], "Matched1", 2.);
+  CreateResiduals(residuals[3], "Matched2", 2.);
+  CreateResiduals(residuals[4], "ClCl", 0.2);
+
+  while (true) {
+
+    if (readNextEvent1) {
+      event1 = ReadNextEvent(inFile1, selectTracks, tracks1);
+      FillResiduals(tracks1, residuals[0]);
+    }
+
+    if (readNextEvent2) {
+      event2 = ReadNextEvent(inFile2, selectTracks, tracks2);
+      FillResiduals(tracks2, residuals[1]);
+    }
+
+    // reaching end of both files
+    if (event1 < 0 && event2 < 0) {
+      break;
+    }
+
+    if (event1 == event2) {
+      // reading the same event --> we can compare tracks
+      CompareTracks(tracks1, tracks2, residuals[4]);
+      FillResiduals(tracks1, residuals[2], true);
+      FillResiduals(tracks2, residuals[3], true);
+      readNextEvent1 = true;
+      readNextEvent2 = true;
+    } else if (event2 < 0 || (event1 >= 0 && event1 < event2)) {
+      // event 2 > event 1 or reaching end of file 2
+      readNextEvent1 = true;
+      readNextEvent2 = false;
+    } else {
+      // event 1 > event 2 or reaching end of file 1
+      readNextEvent1 = false;
+      readNextEvent2 = true;
+    }
+  }
+  
+  gStyle->SetOptStat(1);
+  DrawResiduals(residuals[4], "ClCl");
+  DrawResiduals(residuals[0], residuals[1], "All");
+  DrawResiduals(residuals[2], residuals[3], "Matched");
+
+  inFile1.close();
+  inFile2.close();
+
+}
+
+//_________________________________________________________________________________________________
+int ReadNextEvent(ifstream& inFile, bool selectTracks, std::vector<TrackStruct>& tracks)
+{
+  /// read the next event in the input file
+
+  tracks.clear();
+  
+  int event(-1);
+  if (!inFile.read(reinterpret_cast<char*>(&event), sizeof(int))) {
+    // reaching end of file
+    return -1;
+  }
+  
+  int size(0);
+  inFile.read(reinterpret_cast<char*>(&size), sizeof(int));
+  if (size == 0) {
+    // empty event
+    return event;
+  }
+
+  // read the tracks
+  int nTracks(0);
+  inFile.read(reinterpret_cast<char*>(&nTracks), sizeof(int));
+  tracks.reserve(nTracks);
+  for (Int_t iTrack = 0; iTrack < nTracks; ++iTrack) {
+    tracks.emplace_back();
+    ReadTrack(inFile, tracks.back());
+    if (selectTracks && !IsSelected(tracks.back())) {
+      tracks.pop_back();
+    }
+  }
+
+  return event;
+}
+
+//_________________________________________________________________________________________________
+void ReadTrack(ifstream& inFile, TrackStruct& track)
+{
+  /// read one track from the input file and refit it to get the parameters at each cluster
+
+  TrackParamStruct paramAtVtx{};
+  inFile.read(reinterpret_cast<char*>(&paramAtVtx), sizeof(TrackParamStruct));
+  track.pxpypzm.SetPx(paramAtVtx.px);
+  track.pxpypzm.SetPy(paramAtVtx.py);
+  track.pxpypzm.SetPz(paramAtVtx.pz);
+  track.pxpypzm.SetM(muMass);
+  track.sign = paramAtVtx.sign;
+  inFile.read(reinterpret_cast<char*>(&(track.dca)), sizeof(double));
+  inFile.read(reinterpret_cast<char*>(&(track.rAbs)), sizeof(double));
+
+  inFile.read(reinterpret_cast<char*>(&(track.param)), sizeof(TrackParamStruct));
+
+  inFile.read(reinterpret_cast<char*>(&(track.chi2)), sizeof(double));
+
+  int nClusters(0);
+  inFile.read(reinterpret_cast<char*>(&nClusters), sizeof(int));
+  track.clusters.reserve(nClusters);
+  ClusterStruct clusterIn{};
+  for (Int_t iCl = 0; iCl < nClusters; ++iCl) {
+    inFile.read(reinterpret_cast<char*>(&clusterIn), sizeof(ClusterStruct));
+    track.clusters.emplace_back(clusterIn);
+    track.track.createParamAtCluster(track.clusters.back());
+  }
+
+  try {
+    trackFitter.fit(track.track);
+  } catch (exception const& e) {
+    throw runtime_error(std::string("Track fit failed: ") + e.what());
+  }
+}
+
+//_________________________________________________________________________________________________
+bool IsSelected(TrackStruct& track)
+{
+  /// apply standard track selections + pDCA
+
+  static const double sigmaPDCA23 = 80.;
+  static const double sigmaPDCA310 = 54.;
+  static const double nSigmaPDCA = 6.;
+  static const double relPRes = 0.0004;
+  static const double slopeRes = 0.0005;
+
+  double thetaAbs = TMath::ATan(track.rAbs/505.) * TMath::RadToDeg();
+  if (thetaAbs < 2. || thetaAbs > 10.) {
+    return false;
+  }
+
+  double eta = track.pxpypzm.Eta();
+  if (eta < -4. || eta > -2.5) {
+    return false;
+  }
+
+  double pUncorr = TMath::Sqrt(track.param.px*track.param.px + track.param.py*track.param.py + track.param.pz*track.param.pz);
+  double pDCA = pUncorr * track.dca;
+  double sigmaPDCA = (thetaAbs < 3) ? sigmaPDCA23 : sigmaPDCA310;
+  double pTot = track.pxpypzm.P();
+  double nrp = nSigmaPDCA * relPRes * pTot;
+  double pResEffect = sigmaPDCA / (1. - nrp / (1. + nrp));
+  double slopeResEffect = 535. * slopeRes * pTot;
+  double sigmaPDCAWithRes = TMath::Sqrt(pResEffect*pResEffect + slopeResEffect*slopeResEffect);
+  if (pDCA > nSigmaPDCA * sigmaPDCAWithRes) {
+    return false;
+  }
+
+  return true;
+}
+
+//_________________________________________________________________________________________________
+void CompareTracks(std::vector<TrackStruct>& tracks1, std::vector<TrackStruct>& tracks2, std::vector<TH1*>& histos)
+{
+  /// compare the tracks between the 2 events
+
+  // first look for identical tracks in the 2 events
+  for (auto& track1 : tracks1) {
+    // find a track in the second event identical to track1 and not already matched
+    auto itTrack2 = tracks2.begin();
+    do {
+      itTrack2 = find(itTrack2, tracks2.end(), track1);
+    } while (itTrack2 != tracks2.end() && itTrack2->matchFound && ++itTrack2 != tracks2.end());
+    if (itTrack2 != tracks2.end()) {
+      track1.matchFound = true;
+      itTrack2->matchFound = true;
+      track1.matchIdentical = true;
+      itTrack2->matchIdentical = true;
+      FillResiduals(track1, *itTrack2, histos);
+    }
+  }
+
+  // then look for similar tracks in the 2 events
+  for (auto& track1 : tracks1) {
+    // skip already matched tracks
+    if (track1.matchFound) {
+      continue;
+    }
+    // find a track in the second event similar to track1 and not already matched
+    for (auto& track2 : tracks2) {
+      if (!track2.matchFound && track2.match(track1)) {
+        track1.matchFound = true;
+        track2.matchFound = true;
+        FillResiduals(track1, track2, histos);
+        break;
+      }
+    }
+  }
+}
+
+//_________________________________________________________________________________________________
+void CreateResiduals(std::vector<TH1*>& histos, const char* extension, double range)
+{
+  /// create histograms of cluster-track residuals
+  if (histos.size() == 0) {
+    for (int iSt = 1; iSt <= 5; ++iSt) {
+      histos.emplace_back(new TH1F(Form("resX%sSt%d", extension, iSt),
+                                   Form("#DeltaX Station %d;#DeltaX (cm)", iSt), 2000, -range, range));
+      histos.emplace_back(new TH1F(Form("resY%sSt%d", extension, iSt),
+                                   Form("#DeltaY Station %d;#DeltaY (cm)", iSt), 2000, -range, range));
+    }
+    histos.emplace_back(new TH1F(Form("resX%s", extension), "#DeltaX;#DeltaX (cm)", 2000, -range, range));
+    histos.emplace_back(new TH1F(Form("resY%s", extension), "#DeltaY;#DeltaY (cm)", 2000, -range, range));
+  }
+}
+
+//_________________________________________________________________________________________________
+void FillResiduals(TrackStruct& track1, TrackStruct& track2, std::vector<TH1*>& histos)
+{
+  /// fill histograms of cluster-cluster residuals
+  for (const auto& cl1 : track1.clusters) {
+    for (const auto& cl2 : track2.clusters) {
+      if (cl1.getDEId() == cl2.getDEId()) {
+        histos[cl1.getChamberId() / 2 * 2]->Fill(cl2.getX() - cl1.getX());
+        histos[cl1.getChamberId() / 2 * 2 + 1]->Fill(cl2.getY() - cl1.getY());
+        histos[10]->Fill(cl2.getX() - cl1.getX());
+        histos[11]->Fill(cl2.getY() - cl1.getY());
+      }
+    }
+  }
+}
+
+//_________________________________________________________________________________________________
+void FillResiduals(std::vector<TrackStruct>& tracks, std::vector<TH1*>& histos, bool matched)
+{
+  /// fill histograms of cluster-track residuals
+  for (const auto& track : tracks) {
+    if (!matched || track.matchFound) {
+      for (const auto& param : track.track) {
+        histos[param.getClusterPtr()->getChamberId() / 2 * 2]->Fill(param.getClusterPtr()->getX() - param.getNonBendingCoor());
+        histos[param.getClusterPtr()->getChamberId() / 2 * 2 + 1]->Fill(param.getClusterPtr()->getY() - param.getBendingCoor());
+        histos[10]->Fill(param.getClusterPtr()->getX() - param.getNonBendingCoor());
+        histos[11]->Fill(param.getClusterPtr()->getY() - param.getBendingCoor());
+      }
+    }
+  }
+}
+
+//_________________________________________________________________________________________________
+void DrawResiduals(std::vector<TH1*>& histos, const char* extension)
+{
+  /// draw cluster-cluster residuals
+
+  int nPadsx = (histos.size() + 1) / 2;
+  TCanvas* c = new TCanvas(Form("residual%s", extension), Form("residual%s", extension), 10, 10, nPadsx * 300, 600);
+  c->Divide(nPadsx, 2);
+  int i(0);
+  for (const auto& h : histos) {
+    c->cd((i % 2) * nPadsx + i / 2 + 1);
+    gPad->SetLogy();
+    h->Draw();
+    h->GetXaxis()->SetRangeUser(-0.02, 0.02);
+    ++i;
+  }
+}
+
+//_________________________________________________________________________________________________
+void DrawResiduals(std::vector<TH1*>& histos1, std::vector<TH1*>& histos2, const char* extension)
+{
+  /// draw cluster-track residuals
+
+  TGraphErrors* g[2][2] = {nullptr};
+  const char* dir[2] = {"X", "Y"};
+  int color[2] = {4, 2};
+  for (int i = 0; i < 2; ++i) {
+    for (int j = 0; j < 2; ++j) {
+      g[i][j] = new TGraphErrors(6);
+      g[i][j]->SetName(Form("sigma%s%s%d", dir[i], extension, j));
+      g[i][j]->SetTitle(Form("#sigma%s per station;station ID;#sigma%s (cm)", dir[i], dir[i]));
+      g[i][j]->SetMarkerStyle(kFullDotLarge);
+      g[i][j]->SetMarkerColor(color[j]);
+      g[i][j]->SetLineColor(color[j]);
+    }
+  }
+
+  int nPadsx = (histos1.size() + 1) / 2;
+  TCanvas* c = new TCanvas(Form("residual%s", extension), Form("residual%s", extension), 10, 10, nPadsx * 300, 600);
+  c->Divide(nPadsx, 2);
+  int i(0);
+  for (const auto& h : histos1) {
+    c->cd((i % 2) * nPadsx + i / 2 + 1);
+    gPad->SetLogy();
+    h->SetLineColor(color[0]);
+    h->Draw();
+    auto res1 = GetSigma(h, color[0]);
+    g[i % 2][0]->SetPoint(i / 2, i / 2 + 1., res1.first);
+    g[i % 2][0]->SetPointError(i / 2, 0., res1.second);
+    histos2[i]->SetLineColor(color[1]);
+    histos2[i]->Draw("sames");
+    auto res2 = GetSigma(histos2[i], color[1]);
+    g[i % 2][1]->SetPoint(i / 2, i / 2 + 1., res2.first);
+    g[i % 2][1]->SetPointError(i / 2, 0., res2.second);
+    printf("%s: %f ± %f cm --> %f ± %f cm\n", h->GetName(), res1.first, res1.second, res2.first, res2.second);
+    ++i;
+  }
+
+  TCanvas* c2 = new TCanvas(Form("sigma%s", extension), Form("sigma%s", extension), 10, 10, 600, 300);
+  c2->Divide(2, 1);
+  c2->cd(1);
+  g[0][0]->Draw("ap");
+  g[0][1]->Draw("p");
+  c2->cd(2);
+  g[1][0]->Draw("ap");
+  g[1][1]->Draw("p");
+
+  // add a legend
+  TLegend* lHist = new TLegend(0.2, 0.65, 0.4, 0.8);
+  lHist->SetFillStyle(0);
+  lHist->SetBorderSize(0);
+  lHist->AddEntry(histos1[0], "file 1", "l");
+  lHist->AddEntry(histos2[0], "file 2", "l");
+  c->cd(1);
+  lHist->Draw("same");
+}
+
+//_________________________________________________________________________________________________
+pair<double, double> GetSigma(TH1* h, int color)
+{
+  /// get the dispersion of the histo
+
+  static TF1* fCrystalBall = new TF1("CrystalBall", CrystalBallSymmetric, -2., 2., 5);
+  fCrystalBall->SetLineColor(color);
+
+  if (h->GetEntries() < 10.) {
+    return make_pair(0., 0.);
+  }
+
+  double sigmaTrk = 0.2; // 2 mm
+  double sigmaTrkCut = 4.; // 4 sigma
+
+  // first fit
+  double xMin = -sigmaTrkCut * sigmaTrk;
+  double xMax = sigmaTrkCut * sigmaTrk;
+  fCrystalBall->SetRange(xMin, xMax);
+  fCrystalBall->SetParameters(h->GetEntries(), 0., 0.1, 2., 1.5);
+  fCrystalBall->SetParLimits(1, xMin, xMax);
+  fCrystalBall->SetParLimits(2, 0., 1.);
+  fCrystalBall->FixParameter(3, 1.e6);
+  h->Fit(fCrystalBall, "RNQ");
+
+  // rebin histo
+  int rebin = static_cast<Int_t>(TMath::Min(0.1 * h->GetNbinsX(), TMath::Max(0.3 * fCrystalBall->GetParameter(2) / h->GetBinWidth(1), 1.)));
+  while (h->GetNbinsX() % rebin != 0) {
+    rebin--;
+  }
+  h->Rebin(rebin);
+
+  // second fit
+  fCrystalBall->SetParameter(0, fCrystalBall->GetParameter(0) * rebin);
+  fCrystalBall->ReleaseParameter(3);
+  fCrystalBall->SetParameter(3, 2.);
+  fCrystalBall->SetParameter(4, 1.5);
+  h->Fit(fCrystalBall, "RQ");
+
+  return make_pair(fCrystalBall->GetParameter(2), fCrystalBall->GetParError(2));
+}
+
+//_________________________________________________________________________________________________
+double CrystalBallSymmetric(double* xx, double* par)
+{
+  /// Crystal Ball definition
+
+  ///par[0] = Normalization
+  ///par[1] = mean
+  ///par[2] = sigma
+  ///par[3] = alpha = alpha'
+  ///par[4] = n = n'
+
+  double tp = fabs((xx[0] - par[1]) / par[2]);
+
+  double absAlpha = fabs(par[3]);
+  double ap = pow(par[4] / absAlpha, par[4]) * exp(-0.5 * absAlpha * absAlpha);
+  double bp = par[4] / absAlpha - absAlpha;
+
+  if (tp < absAlpha) {
+    return par[0] * (exp(-0.5 * tp * tp)); // gaussian core
+  } else {
+    return par[0] * (ap / pow(bp + tp, par[4])); //left and right tails
+  }
+}
