a09dd78c60f3ec71c0978d6d0e61e32e0a4e72e2
ppillot <pillot@subatech.in2p3.fr>
2020-11-03 17:15:18 +0100

    follow changes of track format

--- Tracking/ConvertToASCII.C
+++ Tracking/ConvertToASCII.C
@@ -2,11 +2,30 @@
 #include <iostream>
 #include <vector>
 
+#include "DataFormatsMCH/TrackMCH.h"
 #include "MCHBase/ClusterBlock.h"
 #include "MCHBase/TrackBlock.h"
 
 using namespace std;
 using namespace o2::mch;
+
+//_________________________________________________________________________________________________
+struct ClusterStructV1 {
+  float x;      ///< cluster position along x
+  float y;      ///< cluster position along y
+  float z;      ///< cluster position along z
+  float ex;     ///< cluster resolution along x
+  float ey;     ///< cluster resolution along y
+  uint32_t uid; ///< cluster unique ID
+};
+
+//_________________________________________________________________________________________________
+struct TrackAtVtxStruct {
+  TrackParamStruct paramAtVertex{};
+  double dca = 0.;
+  double rAbs = 0.;
+  int mchTrackIdx = 0;
+};
 
 //_________________________________________________________________________________________________
 struct TrackStruct {
@@ -21,6 +40,8 @@ struct TrackStruct {
 //_________________________________________________________________________________________________
 int ReadNextEvent(ifstream& inFile, int version, std::vector<TrackStruct>& tracks, int& size);
 void ReadTrack(ifstream& inFile, TrackStruct& track, int version);
+int ReadNextEventV5(ifstream& inFile, std::vector<TrackStruct>& tracks, int& size);
+void FillTrack(TrackStruct& track, const TrackAtVtxStruct* trackAtVtx, const TrackMCH* mchTrack, std::vector<ClusterStruct>& clusters);
 void WriteTracks(std::vector<TrackStruct>& tracks, ofstream& outFile);
 
 //_________________________________________________________________________________________________
@@ -44,7 +65,12 @@ void ConvertToASCII(string trackFileName, int versionFile)
 
     // get tracks
     int size(0);
-    int event = ReadNextEvent(inFileTrack, versionFile, tracks, size);
+    int event(-1);
+    if (versionFile < 5) {
+      event = ReadNextEvent(inFileTrack, versionFile, tracks, size);
+    } else {
+      event = ReadNextEventV5(inFileTrack, tracks, size);
+    }
     if (event < 0) {
       // reaching end of file
       break;
@@ -110,7 +136,92 @@ void ReadTrack(ifstream& inFile, TrackStruct& track, int version)
   inFile.read(reinterpret_cast<char*>(&nClusters), sizeof(int));
   track.clusters.resize(nClusters);
   for (Int_t iCl = 0; iCl < nClusters; ++iCl) {
-    inFile.read(reinterpret_cast<char*>(&(track.clusters[iCl])), sizeof(ClusterStruct));
+    if (version < 4) {
+      inFile.read(reinterpret_cast<char*>(&(track.clusters[iCl])), sizeof(ClusterStructV1));
+    } else {
+      inFile.read(reinterpret_cast<char*>(&(track.clusters[iCl])), sizeof(ClusterStruct));
+    }
+  }
+}
+
+//_________________________________________________________________________________________________
+int ReadNextEventV5(ifstream& inFile, std::vector<TrackStruct>& tracks, int& size)
+{
+  /// read the next event in the input file
+
+  static int event = -1;
+  ++event;
+
+  tracks.clear();
+
+  int nTracksAtVtx(-1);
+  if (!inFile.read(reinterpret_cast<char*>(&nTracksAtVtx), sizeof(int))) {
+    size = 0;
+    return -1; // reaching end of file
+  }
+  int nMCHTracks(-1);
+  inFile.read(reinterpret_cast<char*>(&nMCHTracks), sizeof(int));
+  int nClusters(-1);
+  inFile.read(reinterpret_cast<char*>(&nClusters), sizeof(int));
+  if (nTracksAtVtx == 0 && nMCHTracks == 0) {
+    size = 3 * sizeof(int);
+    return event; // event is empty
+  }
+
+  // read the tracks at vertex, the MCH tracks and the attached clusters
+  std::vector<TrackAtVtxStruct> tracksAtVtx(nTracksAtVtx);
+  inFile.read(reinterpret_cast<char*>(tracksAtVtx.data()), nTracksAtVtx * sizeof(TrackAtVtxStruct));
+  std::vector<TrackMCH> mchTracks(nMCHTracks);
+  inFile.read(reinterpret_cast<char*>(mchTracks.data()), nMCHTracks * sizeof(TrackMCH));
+  std::vector<ClusterStruct> clusters(nClusters);
+  inFile.read(reinterpret_cast<char*>(clusters.data()), nClusters * sizeof(ClusterStruct));
+
+  if (nTracksAtVtx > 0) {
+
+    // fill the internal track structure based on the provided tracks at vertex
+    tracks.reserve(nTracksAtVtx);
+    for (const auto& trackAtVtx : tracksAtVtx) {
+      tracks.emplace_back();
+      FillTrack(tracks.back(), &trackAtVtx, (nMCHTracks > 0) ? &(mchTracks[trackAtVtx.mchTrackIdx]) : nullptr, clusters);
+    }
+
+  } else {
+
+    // fill the internal track structure based on the MCH tracks extrapolated to the provided vertex
+    tracks.reserve(nMCHTracks);
+    for (const auto& mchTrack : mchTracks) {
+      tracks.emplace_back();
+      FillTrack(tracks.back(), nullptr, &mchTrack, clusters);
+    }
+  }
+
+  size = 3 * sizeof(int) + nTracksAtVtx * sizeof(TrackAtVtxStruct) + nMCHTracks * sizeof(TrackMCH) + nClusters * sizeof(ClusterStruct);
+  return event;
+}
+
+//_________________________________________________________________________________________________
+void FillTrack(TrackStruct& track, const TrackAtVtxStruct* trackAtVtx, const TrackMCH* mchTrack, std::vector<ClusterStruct>& clusters)
+{
+  /// fill the internal track structure from the provided informations
+
+  if (trackAtVtx) {
+    track.paramAtVertex = trackAtVtx->paramAtVertex;
+    track.dca = trackAtVtx->dca;
+    track.rAbs = trackAtVtx->rAbs;
+  }
+
+  if (mchTrack) {
+    track.chi2 = mchTrack->getChi2();
+    track.paramAt1stCluster.x = mchTrack->getX();
+    track.paramAt1stCluster.y = mchTrack->getY();
+    track.paramAt1stCluster.z = mchTrack->getZ();
+    track.paramAt1stCluster.px = mchTrack->getPx();
+    track.paramAt1stCluster.py = mchTrack->getPy();
+    track.paramAt1stCluster.pz = mchTrack->getPz();
+    track.paramAt1stCluster.sign = mchTrack->getSign();
+    track.clusters.insert(track.clusters.end(),
+                          std::make_move_iterator(clusters.begin() + mchTrack->getFirstClusterIdx()),
+                          std::make_move_iterator(clusters.begin() + mchTrack->getLastClusterIdx() + 1));
   }
 }
 
