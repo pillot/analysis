9156496d791e704996d74949782f6c2a8e93b369
ppillot <pillot@subatech.in2p3.fr>
2022-01-26 15:31:02 +0100

    also convert the digits forming the preclusters, if requested

--- PreClustering/ConvertO2PreClustersV2.C
+++ PreClustering/ConvertO2PreClustersV2.C
@@ -9,19 +9,23 @@
 
 #include "AliMUONVCluster.h"
 #include "AliMUONClusterStoreV2.h"
+#include "AliMUONRealDigit.h"
+#include "AliMUONDigitStoreV2R.h"
 
 #include "MCHBase/PreCluster.h"
 #include "DataFormatsMCH/Digit.h"
 
 using namespace o2::mch;
 
-void StorePreclusters(std::vector<PreCluster>& preClusters, std::vector<Digit>& digits, AliMUONClusterStoreV2* clusterStore, bool impl4);
+void StorePreclusters(std::vector<PreCluster>& preClusters, std::vector<Digit>& digits,
+                      AliMUONClusterStoreV2* clusterStore, AliMUONDigitStoreV2R* digitStore, bool impl4);
 uint32_t PadId2DigitId(int deId, int padId, bool impl4);
 
 //------------------------------------------------------------------
-void ConvertO2PreClustersV2(TString inFileName, TString outFileName = "preclusters.v2.root", bool impl4 = true)
+void ConvertO2PreClustersV2(TString inFileName, TString outFileName = "preclusters.v2.root",
+                            TString outDigitFileName = "", bool impl4 = true)
 {
-  /// Convert O2 preclusters to AliRoot clusters
+  /// Convert O2 preclusters to AliRoot clusters and digits (if outDigitFileName != "")
   /// input binary file with the following format:
   ///
   /// Number of preclusters
@@ -46,6 +50,20 @@ void ConvertO2PreClustersV2(TString inFileName, TString outFileName = "precluste
   if (!clusterStore) return;
   if (!clusterStore->Connect(*clusterTree, kTRUE)) return;
 
+  // prepare storage of digits in an AliMUONVCluster format
+  TFile* digitFileOut = nullptr;
+  TTree* digitTree = nullptr;
+  AliMUONDigitStoreV2R* digitStore = nullptr;
+  if (!outDigitFileName.IsNull()) {
+    digitFileOut = TFile::Open(outDigitFileName.Data(), "RECREATE");
+    if (!digitFileOut) return;
+    digitTree = new TTree("TreeD", "Digits");
+    if (!digitTree) return;
+    digitStore = new AliMUONDigitStoreV2R();
+    if (!digitStore) return;
+    if (!digitStore->Connect(*digitTree, kTRUE)) return;
+  }
+
   int nPreClusters(0);
   int nDigits(0);
   std::vector<PreCluster> preClusters{};
@@ -67,22 +85,34 @@ void ConvertO2PreClustersV2(TString inFileName, TString outFileName = "precluste
     inFile.read(reinterpret_cast<char*>(digits.data()), nDigits * sizeof(Digit));
 
     // convert preclusters in AliRoot clusters
-    StorePreclusters(preClusters, digits, clusterStore, impl4);
+    StorePreclusters(preClusters, digits, clusterStore, digitStore, impl4);
 
     clusterTree->Fill();
     clusterStore->Clear();
+    if (digitStore) {
+      digitTree->Fill();
+      digitStore->Clear();
+    }
   }
 
   fileOut->cd();
   clusterTree->Write();
   fileOut->Close();
+  if (digitStore) {
+    digitFileOut->cd();
+    digitTree->Write();
+    digitFileOut->Close();
+  }
   inFile.close();
 }
 
 //------------------------------------------------------------------
-void StorePreclusters(std::vector<PreCluster>& preClusters, std::vector<Digit>& digits, AliMUONClusterStoreV2* clusterStore, bool impl4)
+void StorePreclusters(std::vector<PreCluster>& preClusters, std::vector<Digit>& digits,
+                      AliMUONClusterStoreV2* clusterStore, AliMUONDigitStoreV2R* digitStore, bool impl4)
 {
   /// convert the preclusters in AliRoot clusters and store them in the cluster store
+
+  static AliMUONRealDigit AliRootDigit{};
 
   int clIndex = clusterStore->GetSize();
   std::vector<uint32_t> digitIds(100);
@@ -96,12 +126,23 @@ void StorePreclusters(std::vector<PreCluster>& preClusters, std::vector<Digit>& 
     AliMUONVCluster* cluster = clusterStore->Add(chId, deId, clIndex);
     ++clIndex;
 
-    // add the list of digit Ids
+    // add the list of digit Ids and fill the digit store if any
     digitIds.clear();
     for (auto iDigit = preCluster.firstDigit; iDigit <= preCluster.lastDigit(); ++iDigit) {
-      uint32_t digitId = PadId2DigitId(deId, digits[iDigit].getPadID(), impl4);
+      const auto& o2Digit = digits[iDigit];
+      uint32_t digitId = PadId2DigitId(deId, o2Digit.getPadID(), impl4);
       if (digitId > 0) {
         digitIds.push_back(digitId);
+        if (digitStore) {
+          AliRootDigit.SetUniqueID(digitId);
+          AliRootDigit.SetCharge(o2Digit.getADC());
+          AliRootDigit.ChargeInFC(false);
+          AliRootDigit.SetADC(o2Digit.getADC());
+          AliRootDigit.Saturated(o2Digit.isSaturated());
+          if (!digitStore->Add(AliRootDigit, AliMUONVDigitStore::kDeny)) {
+            printf("digit %d already exist on DE %d\n", digitId, deId);
+          }
+        }
       }
     }
     cluster->SetDigitsId(preCluster.nDigits, digitIds.data());
