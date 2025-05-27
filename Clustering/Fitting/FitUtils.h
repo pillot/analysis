4e4077db77aa5f8660379785953f0dc33667cd4a
Stanislas <134210878+lstanislas@users.noreply.github.com>
2025-05-27 17:11:19 +0200

    add code for toyMC

---
+++ Clustering/Fitting/FitUtils.h
@@ -0,0 +1,158 @@
+#include <array>
+#include <cmath>
+#include <functional>
+#include <string>
+#include <tuple>
+#include <vector>
+
+#include <Fit/Fitter.h>
+#include <Math/Functor.h>
+#include <TMath.h>
+
+#include "DataFormatsMCH/Digit.h"
+#include "MCHMappingInterface/Segmentation.h"
+
+using o2::mch::Digit;
+
+//_________________________________________________________________________________________________
+std::function<double(double, double, double)> MathiesonIntegrate(bool fixK3, double k3 = 0.3)
+{
+  auto init = [](double k3) -> std::tuple<double, double, double> {
+    double sqrtK3 = std::sqrt(k3);
+    double k2 = (TMath::Pi() / 2.) * (1. - 0.5 * sqrtK3);
+    double k4 = 1. / (4 * TMath::ATan(sqrtK3));
+    return std::make_tuple(sqrtK3, k2, k4);
+  };
+
+  auto integrate = [](double min, double max, double sqrtK3, double k2, double k4) -> double {
+    double umin = sqrtK3 * TMath::TanH(k2 * min);
+    double umax = sqrtK3 * TMath::TanH(k2 * max);
+    return 2. * k4 * (TMath::ATan(umax) - TMath::ATan(umin));
+  };
+
+  if (fixK3) {
+    auto [sqrtK3, k2, k4] = init(k3);
+    return [sqrtK3, k2, k4, &integrate](double min, double max, double) -> double {
+      return integrate(min, max, sqrtK3, k2, k4);
+    };
+  } else {
+    return [&init, &integrate](double min, double max, double k3) -> double {
+      auto [sqrtK3, k2, k4] = init(k3);
+      return integrate(min, max, sqrtK3, k2, k4);
+    };
+  }
+}
+
+//_________________________________________________________________________________________________
+std::function<double(double, double, double)> Error(std::string mode, double alpha)
+{
+  if (mode == "LS") {
+    return [](double, double, double adcFit) -> double {
+      return std::sqrt(adcFit);
+    };
+  } else if (mode == "MLS") {
+    return [](double adc, double, double) -> double {
+      return std::sqrt(adc);
+    };
+  } else if (mode == "MC") {
+    return [alpha](double, double sample, double) -> double {
+      return 0.5 * (std::sqrt(sample) + alpha);
+    };
+  } else if (mode == "sADC") {
+    return [alpha](double adc, double, double) -> double {
+      return alpha * std::sqrt(adc);
+    };
+  } else {
+    return [](double, double, double) -> double {
+      return 1.;
+    };
+  }
+}
+
+//_________________________________________________________________________________________________
+ROOT::Fit::FitResult Fit(const std::vector<Digit>& digits, std::array<int, 6> fix, const double* parameters,
+                         std::string errorMode, bool doAsym, double alpha = 0.)
+{
+  const auto& segmentation = o2::mch::mapping::segmentation(digits[0].getDetID());
+  double pitch = (digits[0].getDetID() < 300) ? 0.21 : 0.25; // set pitch value
+
+  // store digit data (x, y, dx, dy, ADC, nSamples) per cathode
+  std::vector<std::array<double, 6>> digitData[2] = {{}, {}};
+  for (const auto& digit : digits) {
+    auto id = digit.getPadID();
+    int iCath = segmentation.isBendingPad(id) ? 0 : 1;
+    digitData[iCath].push_back({segmentation.padPositionX(id), segmentation.padPositionY(id),
+                                segmentation.padSizeX(id) / 2., segmentation.padSizeY(id) / 2.,
+                                static_cast<double>(digit.getADC()), static_cast<double>(digit.getNofSamples())});
+  }
+
+  int iCharge[2] = {4, 5}; // charge parameter index for bending and non-bending
+  int nParam = 6;          // number of parameters to fit
+  double param0[6];        // starting parameters
+  for (int i = 0; i < 6; ++i) {
+    param0[i] = parameters[i];
+  }
+  if (!doAsym) {
+    iCharge[1] = 4;
+    nParam = 5;
+    param0[4] = std::sqrt(parameters[4] * parameters[5]);
+  }
+
+  // error function for chi2 calculation
+  auto error = Error(errorMode, alpha);
+
+  // mathieson functions for chi2 calculation
+  auto mathiesonIntegrateX = MathiesonIntegrate(fix[2] == 1, param0[2]);
+  auto mathiesonIntegrateY = MathiesonIntegrate(fix[3] == 1, param0[3]);
+
+  // chi2 function to be minimized
+  auto chi2Function = [&](const double* par) {
+    double chi2 = 0.;
+    for (int iCath = 0; iCath < 2; ++iCath) {
+      for (const auto& data : digitData[iCath]) {
+        double xmin = (data[0] - par[0] - data[2]) / pitch;
+        double xmax = (data[0] - par[0] + data[2]) / pitch;
+        double ymin = (data[1] - par[1] - data[3]) / pitch;
+        double ymax = (data[1] - par[1] + data[3]) / pitch;
+        double mgIntegrateX = mathiesonIntegrateX(xmin, xmax, par[2]);
+        double mgIntegrateY = mathiesonIntegrateY(ymin, ymax, par[3]);
+        double charge = par[iCharge[iCath]] * mgIntegrateX * mgIntegrateY;
+        double delta = (data[4] - charge) / error(data[4], data[5], charge);
+        chi2 += delta * delta;
+      }
+    }
+    return chi2;
+  };
+
+  ROOT::Fit::Fitter fitter;
+  ROOT::Math::Functor fcn(chi2Function, nParam);
+  fitter.SetFCN(fcn, param0, digits.size(), 1);
+
+  // Parameters config
+  fitter.Config().ParSettings(0).SetName("x");
+  fitter.Config().ParSettings(0).SetStepSize(0.1);
+  fitter.Config().ParSettings(1).SetName("y");
+  fitter.Config().ParSettings(1).SetStepSize(0.05);
+  fitter.Config().ParSettings(2).SetName("kx");
+  fitter.Config().ParSettings(2).SetLimits(0., 1.);
+  fitter.Config().ParSettings(2).SetStepSize(0.01);
+  fitter.Config().ParSettings(3).SetName("ky");
+  fitter.Config().ParSettings(3).SetLimits(0., 1.);
+  fitter.Config().ParSettings(3).SetStepSize(0.01);
+  fitter.Config().ParSettings(4).SetName(doAsym ? "Qt_b" : "Qt");
+  fitter.Config().ParSettings(4).SetStepSize(0.1);
+  if (doAsym) {
+    fitter.Config().ParSettings(5).SetName("Qt_nb");
+    fitter.Config().ParSettings(5).SetStepSize(0.1);
+  }
+
+  // fix parameters if required
+  for (int i = 0; i < nParam; i++) {
+    if (fix[i] == 1) {
+      fitter.Config().ParSettings(i).Fix();
+    }
+  }
+
+  fitter.FitFCN();
+  return fitter.Result();
+}
---
+++ Clustering/Fitting/ToyMCUtils.h
@@ -0,0 +1,136 @@
+#include <algorithm>
+#include <cmath>
+#include <random>
+#include <string>
+#include <vector>
+
+#include "DataFormatsMCH/Digit.h"
+#include "MCHMappingInterface/Segmentation.h"
+#include "MCHSimulation/Response.h"
+
+#include "PreClusterUtils.h"
+
+using o2::mch::Digit;
+using o2::mch::Response;
+
+// generate random numbers
+std::mt19937 mRandom{std::random_device{}()};
+
+//_________________________________________________________________________________________________
+void AddNoise(double& charge, uint32_t nSamples, std::string mode)
+{
+  auto salpha = mode.substr(mode.find('_') + 1);
+  std::replace(salpha.begin(), salpha.end(), 'p', '.');
+  double alpha = std::stod(salpha);
+
+  if (mode.starts_with("MC_")) {
+
+    // for MC th. noise
+    static std::normal_distribution mNoise{0., 0.5};
+    charge += mNoise(mRandom) * (std::sqrt(nSamples) + alpha);
+
+  } else if (mode.starts_with("sADC_")) {
+
+    // sqrt of ADC noise
+    static std::normal_distribution mNoise{0., 1.};
+    charge += mNoise(mRandom) * std::sqrt(charge) * alpha;
+  }
+}
+
+//_________________________________________________________________________________________________
+void GenerateAsymm(double& chargeB, double& chargeNB, std::string mode)
+{
+  double charge = std::sqrt(chargeB * chargeNB);
+
+  if (mode == "none") {
+
+    chargeB = charge;
+    chargeNB = charge;
+
+  } else if (mode.starts_with("gaus_")) {
+
+    mode.erase(0, 5);
+    std::replace(mode.begin(), mode.end(), 'p', '.');
+    auto gamma = std::stod(mode);
+
+    static std::normal_distribution mAsym{0., 0.055};
+    double Y = gamma * mAsym(mRandom);
+    chargeB = charge * exp(Y);
+    chargeNB = charge * exp(-Y);
+
+  } else if (mode == "tripleGaus") {
+    // to be done
+  }
+}
+
+//_________________________________________________________________________________________________
+bool IsAboveThreshold(double charge, std::string mode)
+{
+  if (mode == "gaus") {
+
+    static std::normal_distribution mThreshold{22.2, 2.8};
+    return charge > mThreshold(mRandom);
+
+  } else if (mode == "uniform") {
+
+    return charge > 22.2;
+  }
+
+  return charge > 0.;
+}
+
+//_________________________________________________________________________________________________
+bool IsSelected(const std::vector<Digit>& digits)
+{
+  if (digits.empty()) {
+    return false;
+  }
+
+  auto [sizeX, sizeY] = GetSize(digits);
+  auto [nPadsNB, nPadsB] = GetNPads(digits);
+
+  bool case1 = (sizeX > 2 && sizeY > 2);
+  bool case2 = (sizeY == 2 && sizeX > 2 && sizeX != nPadsNB);
+  bool case3 = (sizeX == 2 && sizeY > 2 && sizeY != nPadsB);
+  bool case4 = (sizeX == 2 && sizeY == 2 && sizeX != nPadsNB && sizeY != nPadsB);
+
+  return case1 || case2 || case3 || case4;
+}
+
+//_________________________________________________________________________________________________
+void TMC(std::vector<Digit>& digits, int32_t time, int deId, double* par, std::string asymm, std::string noise, std::string threshold)
+{
+  static const Response response[] = {{o2::mch::Station::Type1}, {o2::mch::Station::Type2345}};
+  const auto& mSegmentation = o2::mch::mapping::segmentation(deId);
+  int iSt = (deId < 300) ? 0 : 1;
+
+  // generate charge asymmetry if needed
+  if (asymm != "copy") {
+    GenerateAsymm(par[4], par[5], asymm);
+  }
+
+  // borders of charge integration area
+  auto dxy = response[iSt].getSigmaIntegration() * response[iSt].getChargeSpread();
+  auto xMin = par[0] - dxy;
+  auto xMax = par[0] + dxy;
+  auto yMin = par[1] - dxy;
+  auto yMax = par[1] + dxy;
+
+  mSegmentation.forEachPadInArea(xMin, yMin, xMax, yMax, [&](int padid) {
+    auto dx = mSegmentation.padSizeX(padid) * 0.5;
+    auto dy = mSegmentation.padSizeY(padid) * 0.5;
+    auto xPad = mSegmentation.padPositionX(padid) - par[0];
+    auto yPad = mSegmentation.padPositionY(padid) - par[1];
+    double q = response[iSt].chargePadfraction(xPad - dx, xPad + dx, yPad - dy, yPad + dy);
+    if (response[iSt].isAboveThreshold(q)) {
+      q *= mSegmentation.isBendingPad(padid) ? par[4] : par[5];
+      auto nSamples = response[iSt].nSamples(q);
+      if (noise != "none") {
+        AddNoise(q, nSamples, noise);
+      }
+      if (IsAboveThreshold(q, threshold)) {
+        digits.emplace_back(deId, padid, std::round(q), time - 2, std::min(nSamples, 0x3FFU), false);
+      }
+    }
+  });
+}
